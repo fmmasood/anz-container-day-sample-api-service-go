479a46f4ccffb03b4d7799cb742807e8a76c166e
Faraz Masood <26445434+fmmasood@users.noreply.github.com>
2020-04-24 13:34:25 +1000

    update main.go

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World!! I am here !!"}
+		res := &response{Message: "Hello World"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
